b94fdea203bffcdde13b7e4ba6838cdec4718e6a
Sanket <panchmind1@gmail.com>
2024-12-06 17:53:45 +0530

    sport dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ {path:'',redirectTo:'admin-section',pathMatch:'full'},
+
+  { path: 'admin-section', loadChildren: () => import('./admin-section/admin-section.module').then(m => m.AdminSectionModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
